2aad7a3372bc05d524671c87c295fcb401d2965f
Oscar Rivera <riveramoreira04@gmail.com>
2021-07-13 22:02:18 -0600

    - Se corrigen los seeds.

--- app/models/pago.rb
+++ app/models/pago.rb
@@ -7,7 +7,6 @@ class Pago < ApplicationRecord
     usuario_actual = User.find_by_id(user.parent_id)
     while usuario_actual.present?
       monto_comision_usuario = monto_usd * (usuario_actual.plan.porcentaje_comision.to_d / 100)
-      binding.pry
       pago_detalles.build(pago_id: id, user_id: usuario_actual.id, monto_comision: monto_comision_usuario).save!
 
       usuario_actual = User.find_by_id(usuario_actual.parent_id)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 Plan.create!(id: 1, nombre: "Gratis", porcentaje_comision: "0")
-User.create!(email: "riveramoreira04@gmail.com", password: "prueba123", user: "orivera04", telefono: "87501549", plan_id: 1)
+User.create!(email: "riveramoreira04@gmail.com", password: "prueba123", user: "user1", telefono: "87501549", plan_id: 1)
 
 connection = ActiveRecord::Base.connection
 connection.reset_pk_sequence!("plans")
